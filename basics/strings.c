684deafaa734c90b30c8734227d33fad821a44d1
Kunjal Parmar <kunja171205@gmail.com>
2026-09-11 23:21:04 +0530

    Create strings.c

---
+++ basics/strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char sensor_name[] = "IR Sensor";
+
+    printf("Sensor Name: %s\n", sensor_name);
+    printf("Length: %lu\n", strlen(sensor_name));
+
+    return 0;
+}
